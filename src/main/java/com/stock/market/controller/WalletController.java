8c8492bea3d01ccf8c06ca88023bfb55cdc9d06f
povod9 <nazarpovod113@gmail.com>
2026-04-25 13:46:59 +0200

    feat: add controller and service for wallet

--- src/main/java/com/stock/market/controller/WalletController.java
+++ src/main/java/com/stock/market/controller/WalletController.java
@@ -1,4 +1,35 @@
 package com.stock.market.controller;
 
+import com.stock.market.dto.TradeRequest;
+import com.stock.market.entity.WalletEntity;
+import com.stock.market.enums.Type;
+import com.stock.market.service.WalletService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("wallets")
+@Slf4j
 public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PostMapping("/{wallet_id}/stocks/{stock_name}")
+    public ResponseEntity<?> createTrade(
+            @RequestBody TradeRequest req,
+            @PathVariable("wallet_id") String walletId,
+            @PathVariable("stock_name") String stock_name
+    )
+    {
+        walletService.createTrade(req.type(), walletId, stock_name);
+        return ResponseEntity.ok()
+                .build();
+    }
+
+
 }
--- src/main/java/com/stock/market/service/WalletService.java
+++ src/main/java/com/stock/market/service/WalletService.java
@@ -1,4 +1,104 @@
 package com.stock.market.service;
 
+import com.stock.market.dto.TradeRequest;
+import com.stock.market.entity.AuditLogEntity;
+import com.stock.market.entity.StockEntity;
+import com.stock.market.entity.WalletEntity;
+import com.stock.market.entity.WalletStockEntity;
+import com.stock.market.enums.Type;
+import com.stock.market.repository.AuditLogRepository;
+import com.stock.market.repository.StockRepository;
+import com.stock.market.repository.WalletRepository;
+import com.stock.market.repository.WalletStockRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+@Service
 public class WalletService {
+
+    private final StockRepository stockRepository;
+    private final WalletRepository walletRepository;
+    private final AuditLogRepository auditLogRepository;
+    private final WalletStockRepository walletStockRepository;
+
+    public WalletService(StockRepository stockRepository, WalletRepository walletRepository, AuditLogRepository auditLogRepository, WalletStockRepository walletStockRepository) {
+        this.stockRepository = stockRepository;
+        this.walletRepository = walletRepository;
+        this.auditLogRepository = auditLogRepository;
+        this.walletStockRepository = walletStockRepository;
+    }
+
+
+    @Transactional
+    public void createTrade(
+            Type type,
+            String walletId,
+            String stockName
+    )
+    {
+        StockEntity stockEntity = stockRepository.findByStockName(stockName)
+                .orElseThrow(() -> new EntityNotFoundException("Stock with this name doesn't exist: " + stockName));
+
+        WalletEntity walletEntity = walletRepository.findByWalletId(walletId)
+                .orElseGet(() -> {
+                    WalletEntity w = new WalletEntity();
+                    w.setWalletId(walletId);
+                    w.setStocks(new ArrayList<>());
+                    return walletRepository.save(w);
+                });
+
+        walletRepository.save(walletEntity);
+
+        if(type.equals(Type.BUY)) {
+            if (stockEntity.getQuantity() == 0){
+                throw new NoSuchElementException("There is no stock in the bank buy");
+            }
+            WalletStockEntity walletStockEntity = walletStockRepository.findByWalletIdAndStockName(walletId,stockName)
+                    .orElseGet(() -> {
+                        WalletStockEntity newWalletStock = new WalletStockEntity(
+                                null,
+                                walletEntity,
+                                stockName,
+                                1
+                        );
+                        return walletStockRepository.save(newWalletStock);
+                    });
+
+            walletStockEntity.setQuantity(walletStockEntity.getQuantity() + 1);
+            stockEntity.setQuantity(stockEntity.getQuantity() - 1);
+
+            stockRepository.save(stockEntity);
+            walletStockRepository.save(walletStockEntity);
+        }else {
+            WalletStockEntity walletStockEntity = walletStockRepository.findByWalletIdAndStockName(walletId,stockName)
+                    .orElseThrow(() -> new EntityNotFoundException("Cannot find walletStock by:" + walletId + " and " + stockName));
+
+            if(walletStockEntity.getQuantity() == 0){
+                throw new NoSuchElementException("There is no stock in the wallet sell");
+            }
+
+            walletStockEntity.setQuantity(walletStockEntity.getQuantity() - 1);
+            stockEntity.setQuantity(stockEntity.getQuantity() + 1);
+
+            if(walletStockEntity.getQuantity() == 0){
+                walletStockRepository.delete(walletStockEntity);
+            }
+
+            stockRepository.save(stockEntity);
+            walletStockRepository.save(walletStockEntity);
+        }
+
+        AuditLogEntity createAuditLog = new AuditLogEntity(
+                null,
+                type,
+                walletId,
+                stockName
+        );
+
+        auditLogRepository.save(createAuditLog);
+    }
 }
